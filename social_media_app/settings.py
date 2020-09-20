bdf400648a977872afd3f25d7bd22e66660ab326
Jordan Deveau <jordandeveau@Jordans-Macbook.local>
2020-09-19 20:39:22 -0400

    Updated settings

--- social_media_app/settings.py
+++ social_media_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == "True")
 
-ALLOWED_HOSTS = ['photo-sharing--app.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['photosharing-app.herokuapp.com', 'localhost']
 
 
 # Application definition
